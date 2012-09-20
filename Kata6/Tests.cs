8cdf974960d6b46b07df0c3cfdbf427f0c290581
unknown <heleng@mak007.compatibl.com>
2012-09-20 17:10:25 +0300

    Kata 6 project

---
+++ Kata6/Tests.cs
@@ -0,0 +1,77 @@
+﻿#region
+
+using System;
+using FluentAssertions;
+using Xunit;
+
+#endregion
+
+
+namespace Kata6
+{
+    public class TestWrapper
+    {
+        [Fact]
+        public void test_wrap_with_0_column_widht()
+        {
+            var text = "some text lk'l'k";
+            var wrapper = new Wrapper();
+            var result = wrapper.Wrap(text, 0, false);
+            result.Should().Be(text);
+        }
+
+        [Fact]
+        public void test_wrap_with_column_width_19()
+        {
+            const string text = "abc cde sdfddddf some more text text klj;kjkl dddddd";
+            var wrapper = new Wrapper();
+            var result = wrapper.Wrap(text, 19, false);
+            result.Should().Be("abc cde sdfddddf so\nme more text text k\nlj;kjkl dddddd");
+        }
+
+        [Fact]
+        public void test_wrap_with_column_width_19_text_boudaries()
+        {
+            const string text = "aaabc cde sdfddddf someaa more text aatext klj;kjkl dddddd";
+            var wrapper = new Wrapper();
+            var result = wrapper.Wrap(text, 21, true);
+            result.Should().Be("aaabc cde sdfddddf \nsomeaa more text \naatext klj;kjkl \ndddddd");
+        }
+
+        [Fact]
+        public void test_wrap_with_text_lenght_less_then_colwidth()
+        {
+            const string text = "abcd";
+            var wrapper = new Wrapper();
+            var result = wrapper.Wrap(text, 10, false);
+            result.Should().Be("abcd");
+        }
+
+        [Fact]
+        public void test_wrap_with_boundaries_less_20_column_width()
+        {
+            const string text = "abc cde sdfddddf some more text text klj;kjkl dddddd";
+            var wrapper = new Wrapper();
+            Action act =() => wrapper.Wrap(text, 19, true);
+            act.ShouldThrow<Exception>("The column width is rejected as too short for a line wrap");
+        }
+
+        [Fact]
+        public void test_words_longer_then_column_width()
+        {
+            const string text = "abc cde sdfddddf sffdsfomrremoretexttextklj;kjkldddddd";
+            var wrapper = new Wrapper();
+            Action act = () => wrapper.Wrap(text, 22, true);
+            act.ShouldThrow<Exception>("One or more words is longer then column width");
+        }
+
+        [Fact]
+        public void test_words_longer_then_column_width_()
+        {
+            const string text = "abc cde sdfddddf sffdsfomrremoretexttextklj;kjkldddddd";
+            var wrapper = new Wrapper();
+            var result = wrapper.Wrap(text, 22, false);
+            result.Should().Be("abc cde sdfddddf sffds\nfomrremoretexttextklj;\nkjkldddddd");
+        }
+    }
+}
---
+++ Kata6/Wrapper.cs
@@ -0,0 +1,43 @@
+﻿#region 
+
+using System;
+
+#endregion
+
+
+namespace Kata6
+{
+    public class Wrapper
+    {
+        private const short MinColumnWidthForLineWrap = 20;
+
+        public string Wrap(string text, int columnWidth, Boolean breakAtWordBoundaries)
+        {
+            if (breakAtWordBoundaries && columnWidth < MinColumnWidthForLineWrap)
+            {
+                throw new Exception("The column width is rejected as too short for a line wrap");
+            }
+            if (columnWidth == 0 || text.Length < columnWidth)
+            {
+                return text;
+            }
+
+            int indexOfNewLine = 0;
+
+            while (indexOfNewLine < text.Length - columnWidth)
+            {
+                string line = text.Substring(indexOfNewLine, columnWidth);
+                var lastIndexOfSpace = line.LastIndexOf(" ", StringComparison.Ordinal);
+                if (lastIndexOfSpace == -1 && breakAtWordBoundaries)
+                {
+                    throw new Exception("One or more words is longer then column width");
+                }
+                indexOfNewLine += breakAtWordBoundaries ? lastIndexOfSpace + 1 : columnWidth;
+                text = text.Insert(indexOfNewLine, "\n");
+                indexOfNewLine += 1;
+            }
+
+            return text;
+        }
+    }
+}
